ed8ec5f7537781b7bf300fa95f0702b5aa3eaa7d
Jones775 <aljosha.krueger@gmail.com>
2024-05-28 16:16:23 +0200

    Added Buildings

---
+++ Buildings.py
@@ -0,0 +1,43 @@
+
+
+class Building:
+
+    def __init__(self, name, hp, texture):
+        self.name = name
+        self.hp = hp
+        self.texture = texture
+    
+    def loose_hp():
+        pass
+
+class Shipyard(Building):
+
+    def __init__(self):
+        super().__init__("Shipyard", 60)
+
+    def build_ship():
+        pass
+
+    def open_dialogue():
+        pass
+
+
+class Mine(Building):
+
+    def __init__(self):
+        super().__init__("Mine", 40)
+
+    def produce_ressource():
+        pass
+
+class Headquarter(Building):
+
+    def __init__(self):
+        super().__init__("Headquarter", 300)
+
+
+class Base(Building):
+
+    def __init__(self, damage):
+        super().__init__("Base", 50, 10)
+
--- Main.py
+++ Main.py
@@ -8,14 +8,16 @@ screen = pygame.display.set_mode((1500, 800))
 
 
 
-ship_image = pygame.image.load('textures\\spaceship_textures\\Battle_Cruiser.png').convert()
+ship_image = pygame.image.load('textures\\spaceship_textures\\Battle_Cruiser.png').convert_alpha()
 ship_image = pygame.transform.scale(ship_image, (400, 200))
 
 
 
-ship2 = pygame.image.load('textures\\spaceship_textures\\Small_Starfighter.png').convert()
+ship2 = pygame.image.load('textures\\spaceship_textures\\Small_Starfighter.png').convert_alpha()
 ship2 = pygame.transform.scale(ship2, (200, 100))
 
+ship3 = pygame.image.load('textures\\spaceship_textures\\Cargo_Vessel.png').convert_alpha()
+ship3 = pygame.transform.scale(ship3, (200, 100))
 
 running = True
 while running:
@@ -27,7 +29,10 @@ while running:
 
         
 
-
+    screen.fill(pygame.Color("dark blue"))
     screen.blit(ship_image, (200, 100))
     screen.blit(ship2, (500, 500))
+    screen.blit(ship3, (800, 600))
+    
+
     pygame.display.flip()
